5f44480a058fb9de69109d43d443717751a1503f
Dean Chang <dean.chang@freestar.io>
2018-06-14 14:33:13 -0700

    Rebuild framework.

--- FSCommon/FSCommon.h
+++ FSCommon/FSCommon.h
@@ -1,32 +1,32 @@
-////
-////  FSCommon.h
-////  FSCommon
-////
-////  Created by Dean Chang on 5/6/18.
-////  Copyright © 2018 Freestar. All rights reserved.
-////
 //
-//#import <UIKit/UIKit.h>
+//  FSCommon.h
+//  FSCommon
 //
-////! Project version number for FSCommon.
-//FOUNDATION_EXPORT double FSCommonVersionNumber;
+//  Created by Dean Chang on 5/6/18.
+//  Copyright © 2018 Freestar. All rights reserved.
 //
-////! Project version string for FSCommon.
-//FOUNDATION_EXPORT const unsigned char FSCommonVersionString[];
-//
-//// In this header, you should import all the public headers of your framework using statements like #import
-//
-//#import <FSCommon/FSJSONAdapter.h>
-//#import <FSCommon/FSModel.h>
-//#import <FSCommon/FSModel+NSCoding.h>
-//#import <FSCommon/FSValueTransformer.h>
-//#import <FSCommon/FSTransformerErrorHandling.h>
-//#import <FSCommon/FSHTTPSessionManager.h>
-//#import <FSCommon/FSNetworkReachabilityManager.h>
-//#import <FSCommon/FSURLRequestSerialization.h>
-//#import <FSCommon/FSURLResponseSerialization.h>
-//#import <FSCommon/FSURLSessionManager.h>
-//#import <FSCommon/FSLogging.h>
-//#import <FSCommon/FSWeakGCDTimer.h>
-//
-//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FSCommon.
+FOUNDATION_EXPORT double FSCommonVersionNumber;
+
+//! Project version string for FSCommon.
+FOUNDATION_EXPORT const unsigned char FSCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import
+
+#import "FSJSONAdapter.h"
+#import "FSModel.h"
+#import "FSModel+NSCoding.h"
+#import "FSValueTransformer.h"
+#import "FSTransformerErrorHandling.h"
+#import "FSHTTPSessionManager.h"
+#import "FSNetworkReachabilityManager.h"
+#import "FSURLRequestSerialization.h"
+#import "FSURLResponseSerialization.h"
+#import "FSURLSessionManager.h"
+#import "FSLogging.h"
+#import "FSWeakGCDTimer.h"
+
+
