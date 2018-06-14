5c41f28df31edab1ef12a4826beeff9d2b150a69
Dean Chang <dean.chang@freestar.io>
2018-06-14 12:04:52 -0700

    Rebuild framework.

--- build/FSCommon.framework/Headers/FSCommon.h
+++ build/FSCommon.framework/Headers/FSCommon.h
@@ -1,32 +1,32 @@
+////
+////  FSCommon.h
+////  FSCommon
+////
+////  Created by Dean Chang on 5/6/18.
+////  Copyright © 2018 Freestar. All rights reserved.
+////
 //
-//  FSCommon.h
-//  FSCommon
+//#import <UIKit/UIKit.h>
 //
-//  Created by Dean Chang on 5/6/18.
-//  Copyright © 2018 Freestar. All rights reserved.
+////! Project version number for FSCommon.
+//FOUNDATION_EXPORT double FSCommonVersionNumber;
 //
-
-#import <UIKit/UIKit.h>
-
-//! Project version number for FSCommon.
-FOUNDATION_EXPORT double FSCommonVersionNumber;
-
-//! Project version string for FSCommon.
-FOUNDATION_EXPORT const unsigned char FSCommonVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import
-
-#import <FSCommon/FSJSONAdapter.h>
-#import <FSCommon/FSModel.h>
-#import <FSCommon/FSModel+NSCoding.h>
-#import <FSCommon/FSValueTransformer.h>
-#import <FSCommon/FSTransformerErrorHandling.h>
-#import <FSCommon/FSHTTPSessionManager.h>
-#import <FSCommon/FSNetworkReachabilityManager.h>
-#import <FSCommon/FSURLRequestSerialization.h>
-#import <FSCommon/FSURLResponseSerialization.h>
-#import <FSCommon/FSURLSessionManager.h>
-#import <FSCommon/FSLogging.h>
-#import <FSCommon/FSWeakGCDTimer.h>
-
-
+////! Project version string for FSCommon.
+//FOUNDATION_EXPORT const unsigned char FSCommonVersionString[];
+//
+//// In this header, you should import all the public headers of your framework using statements like #import
+//
+//#import <FSCommon/FSJSONAdapter.h>
+//#import <FSCommon/FSModel.h>
+//#import <FSCommon/FSModel+NSCoding.h>
+//#import <FSCommon/FSValueTransformer.h>
+//#import <FSCommon/FSTransformerErrorHandling.h>
+//#import <FSCommon/FSHTTPSessionManager.h>
+//#import <FSCommon/FSNetworkReachabilityManager.h>
+//#import <FSCommon/FSURLRequestSerialization.h>
+//#import <FSCommon/FSURLResponseSerialization.h>
+//#import <FSCommon/FSURLSessionManager.h>
+//#import <FSCommon/FSLogging.h>
+//#import <FSCommon/FSWeakGCDTimer.h>
+//
+//
